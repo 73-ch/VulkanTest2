7ba17b34c009cdec7b6d3b7a300b12c6d7d23048
nami634 <cigarbox.nami@gmail.com>
2018-06-22 19:46:36 +0900

    glm test

--- VulkanTest2/main.cpp
+++ VulkanTest2/main.cpp
@@ -7,9 +7,35 @@
 //
 
 #include <iostream>
+#include <vulkan/vulkan.h>
+#include <glm/glm.hpp>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    VkInstance instance;
+    VkResult result;
+    VkInstanceCreateInfo info = {};
+    uint32_t instance_layer_count;
+    
+    result = vkEnumerateInstanceLayerProperties(&instance_layer_count, nullptr);
+    std::cout << instance_layer_count << " layers found!\n";
+    if (instance_layer_count > 0) {
+        std::unique_ptr<VkLayerProperties[]> instance_layers(new VkLayerProperties[instance_layer_count]);
+        result = vkEnumerateInstanceLayerProperties(&instance_layer_count, instance_layers.get());
+        for (int i = 0; i < instance_layer_count; ++i) {
+            std::cout << instance_layers[i].layerName << "\n";
+        }
+    }
+    
+    const char * names[] = {
+        "VK_LAYER_LUNARG_standard_validation"
+    };
+    info.enabledLayerCount = 1;
+    info.ppEnabledLayerNames = names;
+    
+    result = vkCreateInstance(&info, NULL, &instance);
+    std::cout << "vkCreateInstance result: " << result << "\n";
+    
+    vkDestroyInstance(instance, nullptr);
     return 0;
 }
